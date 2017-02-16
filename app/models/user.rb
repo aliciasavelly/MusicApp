96a16f2221914aa09e6a94b9a22736d29ee8f1e0
aliciasavelly <alicia.savelly@gmail.com>
2017-02-16 10:39:15 -0800

    fill out user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,30 @@
 class User < ApplicationRecord
-
-
+  validates :email, presence: true, uniqueness: true
+  validates :password_digest, presence: {message: "Password can't be blank."}
+  validates :session_token, presence: true, uniqueness: true
+  validates :password, length: {minimum: 6, allow_nil: true}
   after_initialize :ensure_session_token
+
   attr_reader :password
 
-  def self.generate_session_token
+  def self.find_by_credentials(email, password)
+    user = User.find_by(email: email)
+    return user if user && user.is_password?(password)
+    nil
+  end
 
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
   end
 
   def reset_session_token!
-
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session_token
   end
 
   def ensure_session_token
-
+    self.session_token ||= User.generate_session_token
   end
 
   def password=(password)
@@ -22,6 +33,6 @@ class User < ApplicationRecord
   end
 
   def is_password?(password)
-
+    BCrypt::Password.new(self.password_digest) == password
   end
 end
