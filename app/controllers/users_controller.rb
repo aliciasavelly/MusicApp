3fd90aad972319d69c861e707446d4599103b464
aliciasavelly <alicia.savelly@gmail.com>
2017-02-16 10:49:23 -0800

    set up users controller, sessions controller, and routes for the controllers

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if user.save!
+      redirect_to users_url
+    else
+      flash.now[:errors] = @users.errors.full_messages
+      render :new
+    end
+  end
+
+  def show
+    render @user
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:password, :email)
+  end
+end
