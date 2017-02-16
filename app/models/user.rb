e031e04f0a1c2459d6653ac08815f6e2cd46f903
aliciasavelly <alicia.savelly@gmail.com>
2017-02-16 10:25:49 -0800

    set up skeleton for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,27 @@
 class User < ApplicationRecord
+
+
+  after_initialize :ensure_session_token
+  attr_reader :password
+
+  def self.generate_session_token
+
+  end
+
+  def reset_session_token!
+
+  end
+
+  def ensure_session_token
+
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+
+  end
 end
