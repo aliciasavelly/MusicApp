a51e95383cf138830a150d22f6c5098b78254653
aliciasavelly <alicia.savelly@gmail.com>
2017-02-16 11:00:55 -0800

    add to controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def create
+    user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+
+    if user.nil?
+      flash.now[:errors] = user.errors.full_messages
+      render :new
+    elsif logged_in?(user)
+      redirect_to users_url
+    else
+      log_in_user!(user)
+      redirect_to users_url
+    end
+  end
+
+  
 end
