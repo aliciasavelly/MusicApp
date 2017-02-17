21c19e9ed377cfedfe19c392119d4d1239e8884b
aliciasavelly <alicia.savelly@gmail.com>
2017-02-16 16:45:43 -0800

    allow editing, showing, and creating of bands

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -21,21 +21,30 @@ class BandsController < ApplicationController
   end
 
   def edit
-
+    @band = Band.find(params[:id])
+    render :edit
   end
 
   def show
-    # @band = Band.find_by_name(band_params)
     @band = Band.find(params[:id])
     render :show
   end
 
   def update
+    @band = Band.find(params[:id])
 
+    if @band.update_attributes(band_params)
+      redirect_to band_url(@band)
+    else
+      flash.now[:errors] = @band.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
-
+    @band = Band.find(params[:id])
+    @band.destroy
+    redirect_to bands_url
   end
 
   private
