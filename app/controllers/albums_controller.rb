b9f8b0621239df07eddff9574211ca6ad057a966
aliciasavelly <alicia.savelly@gmail.com>
2017-02-20 18:47:16 -0800

    edit albums controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -2,7 +2,7 @@ class AlbumsController < ApplicationController
 
   def new
     @band = Band.find(params[:band_id])
-    @album = Album.new(band_id: params[:band_id])
+    @album = Album.new
     render :new
   end
 
